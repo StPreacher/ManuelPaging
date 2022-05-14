10af549ce72414e2717639a761cc72cfee781dc2
mucahid <mucahidd3@gmail.com>
2022-05-14 16:39:26 +0300

    implement dagger hilt to project

---
+++ app/src/main/java/com/scorp/casestudy/BaseApplication.kt
@@ -0,0 +1,14 @@
+package com.scorp.casestudy
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+/**
+ * @author Mucahid Dogan
+ * @since 14.05.22
+ *
+ * mucahidd3@gmail.com
+ */
+@HiltAndroidApp
+class BaseApplication : Application() {
+}
--- app/src/main/java/com/scorp/casestudy/MainActivity.kt
+++ app/src/main/java/com/scorp/casestudy/MainActivity.kt
@@ -2,10 +2,19 @@ package com.scorp.casestudy
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import dagger.hilt.android.AndroidEntryPoint
 
+/**
+ * @author Mucahid Dogan
+ * @since 14.05.22
+ *
+ * mucahidd3@gmail.com
+ */
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
